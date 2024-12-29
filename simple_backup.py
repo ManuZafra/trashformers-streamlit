ba143c5d33670c65876f199f3b93b9dc5a90f441
Manuel Zafra <mzafraox@gmail.com>
2024-12-29 23:34:01 +0100

    wiiii

--- simple_backup.py
+++ simple_backup.py
@@ -13,7 +13,7 @@ import streamlit as st
 
 
 if __name__ == "__main__":
-    st.title("∑©√ß µ„„„!        qué dise cabesa? esta es mi página web en pruebas. wiiiiiiii.  interneeeeeeee. funcionaaaaaaa. duboskyyyyyyyy. wiiiiiiiiiii")
+    st.title("∑©√ß µ„„„!        qué dise cabesa? esta es mi página web en pruebas. wiiiiiiii.  interneeeeeeee. funcionaaaaaaa. duboskyyyyyyyy. wiiiiiiiiiii. micael es nuestro pastor")
 
 def get_base64(bin_file):
     with open(bin_file, 'rb') as f:  # Ouvre le fichier en mode binaire.
